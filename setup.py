6f92711fafda87ccee03f5a567136360b30f7ff7
Peter Zhou <peter.zhou@yale.edu>
2019-03-17 16:27:15 -0400

    publish 0.0.3

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-import pathlib
 from setuptools import setup, find_packages
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
     name="langapi",
-    version="0.0.1",
+    version="0.0.3",
     description="Python module for Lang API - see https://docs.langapi.co/ for more information.",
-    long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/peterlzhou/langapi-python",
     author="Peter Zhou",
